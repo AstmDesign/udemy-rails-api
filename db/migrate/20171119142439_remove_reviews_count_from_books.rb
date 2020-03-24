6bb46391a105317dc788a46c3ea19e96290e786e
Astm <astm_desig@hotmail.com>
2020-03-24 11:25:30 +0200

    Update 20171119142439_remove_reviews_count_from_books.rb

--- db/migrate/20171119142439_remove_reviews_count_from_books.rb
+++ db/migrate/20171119142439_remove_reviews_count_from_books.rb
@@ -1,5 +1,6 @@
 class RemoveReviewsCountFromBooks < ActiveRecord::Migration[5.1]
   def change
-    remove_column :books, :reviews_count
+    # you have to comment the below line because there's not field called reviews_count in the books
+    # remove_column :books, :reviews_count
   end
 end
